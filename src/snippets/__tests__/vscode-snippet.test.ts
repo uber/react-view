78002790240c8c6e1d56118ccd7400b29acbfacd
Vojtech Miksu <vojtech@uber.com>
2020-01-27 15:43:54 -0800

    Add vscode snippet generator

---
+++ src/snippets/__tests__/vscode-snippet.test.ts
@@ -0,0 +1,333 @@
+/*
+Copyright (c) 2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+import {vscodeSnippet, PropTypes} from '../../index';
+
+describe('vscodeSnippet imports', () => {
+  test('no imports', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+      })['Button import']
+    ).toBeUndefined();
+  });
+  test('single default import', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        imports: {
+          'your-button-component': {
+            default: 'Button',
+          },
+        },
+      })['Button import']
+    ).toEqual({
+      body: ["import ${1:Button} from 'your-button-component';"],
+      description: 'Base Button import.',
+      prefix: ['Button import'],
+      scope: 'javascript,javascriptreact,typescript,typescriptreact',
+    });
+  });
+  test('single named import', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        imports: {
+          'your-button-component': {
+            named: ['SIZE'],
+          },
+        },
+      })['Button import']
+    ).toEqual({
+      body: ["import ${1:{${2:SIZE}\\}} from 'your-button-component';"],
+      description: 'Base Button import.',
+      prefix: ['Button import'],
+      scope: 'javascript,javascriptreact,typescript,typescriptreact',
+    });
+  });
+  test('multiple named imports', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        imports: {
+          'your-button-component': {
+            named: ['SIZE', 'KIND'],
+          },
+        },
+      })['Button import']
+    ).toEqual({
+      body: [
+        "import ${1:{${2:SIZE, }${3:KIND}\\}} from 'your-button-component';",
+      ],
+      description: 'Base Button import.',
+      prefix: ['Button import'],
+      scope: 'javascript,javascriptreact,typescript,typescriptreact',
+    });
+  });
+  test('default import and named imports', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        imports: {
+          'your-button-component': {
+            named: ['SIZE', 'KIND'],
+            default: 'Button',
+          },
+        },
+      })['Button import']
+    ).toEqual({
+      body: [
+        "import ${1:Button, }${2:{${3:SIZE, }${4:KIND}\\}} from 'your-button-component';",
+      ],
+      description: 'Base Button import.',
+      prefix: ['Button import'],
+      scope: 'javascript,javascriptreact,typescript,typescriptreact',
+    });
+  });
+  test('multiple from sources', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        imports: {
+          'your-button-component': {
+            named: ['SIZE', 'KIND'],
+            default: 'Button',
+          },
+          'your-toast-component': {
+            default: 'Toast',
+          },
+        },
+      })['Button import']
+    ).toEqual({
+      body: [
+        "import ${1:Button, }${2:{${3:SIZE, }${4:KIND}\\}} from 'your-button-component';",
+        "import ${5:Toast} from 'your-toast-component';",
+      ],
+      description: 'Base Button import.',
+      prefix: ['Button import'],
+      scope: 'javascript,javascriptreact,typescript,typescriptreact',
+    });
+  });
+  test('prop imports', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        props: {
+          size: {
+            value: false,
+            type: PropTypes.Boolean,
+            description: '',
+            imports: {
+              'your-button-component': {
+                named: ['SIZE'],
+              },
+            },
+          },
+          kind: {
+            value: false,
+            type: PropTypes.Boolean,
+            description: '',
+            imports: {
+              'your-button-component': {
+                named: ['KIND'],
+              },
+            },
+          },
+        },
+      })['Button import']
+    ).toEqual({
+      body: [
+        "import ${1:{${2:SIZE, }${3:KIND}\\}} from 'your-button-component';",
+      ],
+      description: 'Base Button import.',
+      prefix: ['Button import'],
+      scope: 'javascript,javascriptreact,typescript,typescriptreact',
+    });
+  });
+});
+describe('vscodeSnippet component', () => {
+  test('no props', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+      })
+    ).toEqual({
+      Button: {
+        body: ['<Button', '/>'],
+        description: 'Base Button component.',
+        prefix: ['Button'],
+        scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      },
+    });
+  });
+  test('generic prop', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        props: {
+          onClick: {
+            value: '() => alert("click")',
+            type: PropTypes.Function,
+            description: `Function called when button is clicked.`,
+          },
+        },
+      })
+    ).toEqual({
+      Button: {
+        body: ['<Button', '  ${1:onClick={${2:() => alert("click")}\\}}', '/>'],
+        description: 'Base Button component.',
+        prefix: ['Button'],
+        scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      },
+    });
+  });
+  test('boolean prop', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        props: {
+          disabled: {
+            value: false,
+            type: PropTypes.Boolean,
+            description: 'Indicates that the button is disabled',
+          },
+        },
+      })
+    ).toEqual({
+      Button: {
+        body: ['<Button', '  ${1:disabled}', '/>'],
+        description: 'Base Button component.',
+        prefix: ['Button'],
+        scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      },
+    });
+  });
+  test('enum prop', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        props: {
+          size: {
+            value: 'SIZE.default',
+            defaultValue: 'SIZE.default',
+            options: {default: 'default', big: 'big'},
+            type: PropTypes.Enum,
+            description: 'Defines the size of the button.',
+            imports: {},
+          },
+        },
+      })
+    ).toEqual({
+      Button: {
+        body: ['<Button', '  ${1:size={${2|SIZE.default,SIZE.big|}\\}}', '/>'],
+        description: 'Base Button component.',
+        prefix: ['Button'],
+        scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      },
+    });
+  });
+  test('enum prop with enumName', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        props: {
+          size: {
+            value: 'CUSTOM.default',
+            defaultValue: 'CUSTOM.default',
+            enumName: 'CUSTOM',
+            options: {big: 'big', default: 'default'},
+            type: PropTypes.Enum,
+            description: 'Defines the size of the button.',
+            imports: {},
+          },
+        },
+      })
+    ).toEqual({
+      Button: {
+        body: [
+          '<Button',
+          '  ${1:size={${2|CUSTOM.default,CUSTOM.big|}\\}}',
+          '/>',
+        ],
+        description: 'Base Button component.',
+        prefix: ['Button'],
+        scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      },
+    });
+  });
+  test('enum prop without imports', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        props: {
+          size: {
+            value: 'default',
+            defaultValue: 'default',
+            options: {big: 'big', default: 'default'},
+            type: PropTypes.Enum,
+            description: 'Defines the size of the button.',
+          },
+        },
+      })
+    ).toEqual({
+      Button: {
+        body: ['<Button', '  ${1:size={${2|default,big|}\\}}', '/>'],
+        description: 'Base Button component.',
+        prefix: ['Button'],
+        scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      },
+    });
+  });
+  test('enum prop with values containing a dash', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        props: {
+          size: {
+            value: "SIZE['default-val']",
+            defaultValue: "SIZE['default-val']",
+            options: {['default-val']: 'default-val', ['big-val']: 'big-val'},
+            type: PropTypes.Enum,
+            description: 'Defines the size of the button.',
+            imports: {},
+          },
+        },
+      })
+    ).toEqual({
+      Button: {
+        body: [
+          '<Button',
+          "  ${1:size={${2|SIZE['default-val'],SIZE['big-val']|}\\}}",
+          '/>',
+        ],
+        description: 'Base Button component.',
+        prefix: ['Button'],
+        scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      },
+    });
+  });
+  test('children prop', () => {
+    expect(
+      vscodeSnippet({
+        componentName: 'Button',
+        props: {
+          children: {
+            value: 'Hey',
+            type: PropTypes.String,
+            description: 'Foo',
+          },
+        },
+      })
+    ).toEqual({
+      Button: {
+        body: ['<Button', '>', '  ${1:Hey}', '</Button>'],
+        description: 'Base Button component.',
+        prefix: ['Button'],
+        scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      },
+    });
+  });
+});
---
+++ src/snippets/vscode-snippet.ts
@@ -0,0 +1,148 @@
+/*
+Copyright (c) 2019 Uber Technologies, Inc.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
+import {clone} from '../utils';
+import {TProp, TImportsConfig, PropTypes} from '../';
+import {addToImportList} from '../code-generator';
+
+const joinNamed = (items: string[] | undefined, ctr: number) => {
+  if (!items) return '';
+  let output = `\${${ctr++}:{`;
+  for (let i = 0; i < items.length; i++) {
+    if (i !== items.length - 1) {
+      output += `\${${ctr++}:${items[i]}, }`;
+    } else {
+      output += `\${${ctr++}:${items[i]}}`;
+    }
+  }
+  return `${output}\\}}`;
+};
+
+type TVscodeSnippetOutput = {
+  [key: string]: {
+    body: string[];
+    description: string;
+    prefix: string[];
+    scope: string;
+  };
+};
+
+type TVscodeSnippet = (props: {
+  componentName: string;
+  imports?: TImportsConfig;
+  props?: {[key: string]: TProp<any>};
+}) => TVscodeSnippetOutput;
+
+const getImportBody = (
+  imports?: TImportsConfig,
+  props?: {[key: string]: TProp<any>}
+) => {
+  const importList: TImportsConfig = imports ? clone(imports) : {};
+  // prop level imports (typically enums related) that are displayed
+  // only when the prop is being used
+  props &&
+    Object.values(props).forEach(prop => {
+      if (prop.imports) {
+        addToImportList(importList, prop.imports);
+      }
+    });
+  const importBody: string[] = [];
+  let ctr = 1;
+  for (const from in importList) {
+    const def = importList[from].default;
+    const named =
+      Array.isArray(importList[from].named) &&
+      (importList[from].named as string[]).length > 0
+        ? importList[from].named
+        : undefined;
+    const defaultImport = def ? `\${${ctr++}:${def}${named ? ', }' : '}'}` : '';
+    importBody.push(
+      `import ${defaultImport}${joinNamed(named, ctr)} from '${from}';`
+    );
+    if (named) {
+      ctr += named.length + 1;
+    }
+  }
+  return importBody;
+};
+
+const getComponentBody = (
+  componentName: string,
+  props?: {[key: string]: TProp<any>}
+) => {
+  let ctr2 = 1;
+  const componentBody = [`<${componentName}`];
+  if (props) {
+    for (const propName in props) {
+      if (props[propName].hidden) continue;
+      if (propName === 'children' || propName === 'overrides') continue;
+      if (props[propName].type === PropTypes.Boolean) {
+        const row = `  \${${ctr2++}:${propName}}`;
+        componentBody.push(row);
+      } else if (props[propName].type === PropTypes.Enum) {
+        const enumName = props[propName].imports
+          ? props[propName].enumName || propName.toUpperCase()
+          : null;
+        const opts = Object.values(props[propName].options)
+          .map((opt: any) =>
+            enumName
+              ? opt.includes('-')
+                ? `${enumName}['${opt}']`
+                : `${enumName}.${opt}`
+              : opt
+          )
+          .filter(opt => opt !== props[propName].defaultValue);
+        if (props[propName].defaultValue) {
+          opts.unshift(props[propName].defaultValue as string);
+        }
+        const row = `  \${${ctr2++}:${propName}={\${${ctr2++}|${opts.join(
+          ','
+        )}|}\\}}`;
+        componentBody.push(row);
+      } else {
+        const row = `  \${${ctr2++}:${propName}={\${${ctr2++}:${props[propName]
+          .defaultValue || props[propName].value}}\\}}`;
+        componentBody.push(row);
+      }
+    }
+    if (props['children'] && !props['children'].hidden) {
+      componentBody.push('>');
+      componentBody.push(`  \${${ctr2++}:${props['children'].value}}`);
+      componentBody.push(`</${componentName}>`);
+    } else {
+      componentBody.push(`/>`);
+    }
+  } else {
+    componentBody.push(`/>`);
+  }
+  return componentBody;
+};
+
+const vscodeSnippet: TVscodeSnippet = ({componentName, imports, props}) => {
+  const output: TVscodeSnippetOutput = {};
+
+  const importBody = getImportBody(imports, props);
+  if (importBody.length > 0) {
+    output[`${componentName} import`] = {
+      scope: 'javascript,javascriptreact,typescript,typescriptreact',
+      prefix: [`${componentName} import`],
+      description: `Base ${componentName} import.`,
+      body: importBody,
+    };
+  }
+
+  output[`${componentName}`] = {
+    scope: 'javascript,javascriptreact,typescript,typescriptreact',
+    prefix: [`${componentName}`],
+    description: `Base ${componentName} component.`,
+    body: getComponentBody(componentName, props),
+  };
+
+  return output;
+};
+
+export default vscodeSnippet;
